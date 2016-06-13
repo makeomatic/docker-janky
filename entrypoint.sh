2fefbad4594effa6c536d4e3cfc38915c1ebded0
AVVS <v@aminev.me>
2016-06-13 16:47:27 +0300

    avoid reload issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,28 @@ set -e
 #set the DEBUG env variable to turn on debugging
 [[ -n "$DEBUG" ]] && set -x
 
+pid=0
+
 # launch migration
 bundle exec rake db:migrate
 
+# SIGTERM & SIGINT -handler
+# https://medium.com/@gchudnov/trapping-signals-in-docker-containers-7a57fdda7d86#.2rkt303t7
+term_handler() {
+  if [ $pid -ne 0 ]; then
+    kill -SIGTERM "$pid"
+    wait "$pid"
+  fi
+  exit 0; # we drop 0, because SIGINT is just a reload
+}
+
+trap term_handler SIGTERM SIGINT
+
 # launch db
-bundle exec $@
+bundle exec $@ & pid="$!"
+
+# wait indefinetely
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
